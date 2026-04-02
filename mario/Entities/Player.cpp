8a33d5634bebd5dbae0012728faf4b0475982d1a
tharun-adithiya <tharunadithiyav.s@gmail.com>
2026-04-03 00:36:53 +0530

    Added Gravity for the player. Refactored Update to have different methods for handling input, movement logic and gravity logic

--- mario/Entities/Player.cpp
+++ mario/Entities/Player.cpp
@@ -5,21 +5,34 @@
 Player::Player()
 {
     position = Vector2D(100, 100);
+    velocity = Vector2D(0, 0);
     width = 50;
     height = 50;
 }
 
 void Player::Update(float dt)
 {
-    if (IsKeyDown(KEY_D))
-    {
-        position.x += speed * dt;
-    }
+    ApplyGravity(dt);
+    HandleInput();
+    Move(dt);
+}
+void Player::HandleInput()
+{
+    if (IsKeyDown(KEY_D)) inputAxisX = 1;
+    
+    else if (IsKeyDown(KEY_A)) inputAxisX = -1;
 
-    if (IsKeyDown(KEY_A))
-    {
-        position.x -= speed * dt;
-    }
+    else inputAxisX = 0;
+}
+void Player::Move(float dt)               // This function updates the player's position based on the input axis and speed, allowing for horizontal movement
+{
+    velocity.x = inputAxisX * speed;
+    position += velocity * dt;
+}
+void Player::ApplyGravity(float dt)     // This function applies gravity to the player, affecting the vertical velocity and position
+{
+    velocity.y += gravity * dt;        // 9.81 is the gravitation constant for earth, you can adjust it for different gravity strength 
+    position += velocity * dt;
 }
 
 void Player::Render()
--- mario/Entities/Player.h
+++ mario/Entities/Player.h
@@ -5,12 +5,17 @@ class Player
 {
     private:
         Vector2D position;
+        Vector2D velocity;
         float height;
         float width;
+        float gravity = 9.81;
         float speed=100;
+        int inputAxisX=0;
     public:
-        
         Player();
         void Update(float dt);
+        void HandleInput();
         void Render();
+        void Move(float dt);
+        void ApplyGravity(float dt);
 };
